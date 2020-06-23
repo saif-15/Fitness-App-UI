4d4772bec861b9a10ed922aa8554b8fe4ac84216
shady <saifulhaq.15@outlook.com>
2020-06-23 22:11:26 +0500

    first page completed

--- lib/list.dart
+++ lib/list.dart
@@ -3,121 +3,128 @@ import 'package:flutter/material.dart';
 class ListData extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Column(
       children: <Widget>[
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Text(
-              "February 2020",
-              style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  "February 2020",
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "On Going",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "January 2020",
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Failed",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "December 2019",
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "Completed",
+                  style: TextStyle(
+                      color: Colors.grey,
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+              ],
             ),
-            Text(
-              "On Going",
-              style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.normal),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "January 2020",
-              style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Failed",
-              style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.normal),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Text(
-              "December 2019",
-              style: TextStyle(
-                  color: Colors.blue,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),
-            Text(
-              "Completed",
-              style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 16,
-                  fontWeight: FontWeight.normal),
-            ),
-            SizedBox(
-              height: 10,
-            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Text(
+                  "Lose weight",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87),
+                ),
+                Text(
+                  "4.4 klgf/7 kg",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Running per month",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87),
+                ),
+                Text(
+                  "15.3 km/20,0 km",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.grey,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Average steps per day",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87),
+                ),
+                Text(
+                  "20000/22000",
+                  style: TextStyle(
+                    fontSize: 15,
+                    fontWeight: FontWeight.normal,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            )
           ],
         ),
-        Column(
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: <Widget>[
-              Text(
-                "Lose weight",
-                style: TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.bold,
-        color: Colors.black87),
-              ),
-              Text(
-                "4.4 klgf/7 kg",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.grey,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Running per month",
-                style: TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.bold,
-        color: Colors.black87),
-              ),
-              Text(
-                "15.3 km/20,0 km",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.grey,
-                ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                "Average steps per day",
-                style: TextStyle(
-        fontSize: 16,
-        fontWeight: FontWeight.bold,
-        color: Colors.black87),
-              ),
-              Text(
-                "20000/22000",
-                style: TextStyle(
-                  fontSize: 15,
-                  fontWeight: FontWeight.normal,
-                  color: Colors.grey,
-                ),
-              ),
-            ],
-          )
+        SizedBox(
+          height: 10.0,
+        ),
       ],
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,81 @@ import 'package:flutter/material.dart';
 
 import 'list.dart';
 
-void main()=>runApp(MyApp());
+void main() => runApp(MaterialApp(
+      home: MyApp(),
+      debugShowCheckedModeBanner: false,
+    ));
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+  var animationController;
+
+  @override
+  void initState() {
+    animationController = AnimationController(vsync: this,duration: Duration(seconds: 1));
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          body:Column(
+    var width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Column(
             children: <Widget>[
-              TopBar(),
-              RadialProgressBar(),
-              SizedBox(height: 20,),
-              ListData(),
+              Stack(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      TopBar(),
+                      RadialProgressBar(),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      ListData(),
+                    ],
+                  ),
+                ],
+              ),
             ],
           ),
-        ),
+          Positioned(
+              left: width / 2 - 25,
+              bottom: 30.0,
+              child: Center(
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.red, width: 4.0)),
+                  child: IconButton(
+                      icon: AnimatedIcon(
+                        icon: AnimatedIcons.menu_close,
+                        progress: animationController.view,
+                      ),
+                      onPressed: () {
+                        onIconPressed();
+                      }),
+                ),
+              ))
+        ],
+      ),
     );
+  }
+
+  void onIconPressed() {
+    animationStatus
+        ? animationController.reverse()
+        : animationController.forward();
+  }
+
+  bool get animationStatus {
+    return animationController.status == AnimationStatus.completed;
   }
 }
